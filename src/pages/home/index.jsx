3f74a12db9886cd1e4504cd79c0e9e70c85eb989
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-11-15 19:03:37 -0300

    feat: get context value and update loading system

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Button, Text } from '@rneui/themed';
 import PageContainer from '../../components/page-container';
+import Loader from '../../components/loader';
+import { StoreContext } from '../../store/context';
 
 const Home = ({ navigation }) => {
   const [loading, setLoading] = React.useState(true);
+  const { farmer, checklist } = useContext(StoreContext);
+
+  useEffect(() => {
+    console.log('farmer :', farmer);
+    console.log('checklist :', checklist);
+  }, []);
 
   return (
-    <PageContainer>
-      {loading ? (
-        <Text>Loading...</Text>
-      ) : (<>
-        <Text>Hello World!!</Text>
-        <Button
-          title="Go to Checklist Page"
-          onPress={() =>
-            navigation.navigate('Checklist', {})
-          }
-          />
-      </>)}
+    <PageContainer loading={loading}>
+      <Text>Hello World!!</Text>
+      <Button
+        title="Go to Checklist Page"
+        onPress={() =>
+          navigation.navigate('Checklist', {})
+        }
+        />
     </PageContainer>
   );
 };
