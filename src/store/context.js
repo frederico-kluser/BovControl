b7d2720020c9f25a195b532b75ce08f331564a71
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-11-15 19:02:56 -0300

    feat: define initial global contextAPI value

--- src/store/context.js
+++ src/store/context.js
@@ -3,5 +3,17 @@ import React from 'react';
 export const StoreContext = React.createContext(null);
 
 export const StoreProvider = ({children}) => {
-  return <StoreContext.Provider value={{}}>{children}</StoreContext.Provider>;
+  const [farmer, setFarmer] = React.useState(null);
+  const [checklist, setChecklist] = React.useState(null);
+
+  const value = {
+    farmer,
+    setFarmer,
+    checklist,
+    setChecklist,
+  };
+
+  return (
+    <StoreContext.Provider value={value}>{children}</StoreContext.Provider>
+  );
 };
