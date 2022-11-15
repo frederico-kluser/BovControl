c399189f7cc2f0910ed9e850a5d4154d9aa8b43f
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-11-15 13:35:12 -0300

    fix: endOfLine error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
