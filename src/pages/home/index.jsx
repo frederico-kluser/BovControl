3b77f880138f9b53f782a3011308bc87cd314132
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-11-15 20:31:55 -0300

    test: run modal on home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,25 @@
 import React, { useContext, useEffect } from 'react';
 import { Button, Text } from '@rneui/themed';
 import PageContainer from '../../components/page-container';
-import Loader from '../../components/loader';
 import { StoreContext } from '../../store/context';
 
 const Home = ({ navigation }) => {
   const [loading, setLoading] = React.useState(true);
-  const { farmer, checklist } = useContext(StoreContext);
+  const { farmer, checklist, showModal } = useContext(StoreContext);
 
   useEffect(() => {
     console.log('farmer :', farmer);
     console.log('checklist :', checklist);
+    setTimeout(() => {
+
+      setLoading(false);
+      if (!farmer) {
+        showModal('test title', 'test content', () => {
+          console.log('onBackdropPress');
+        });
+      }
+      
+    }, 1000);
   }, []);
 
   return (
