34e863a1a409cc80315ee0b5bd82b0f216310b8a
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-11-17 09:51:15 -0300

    refactor: update page-container extension

--- src/components/page-container/index.js
+++ src/components/page-container/index.js
@@ -3,15 +3,10 @@ import React from 'react';
 import Loader from '../loader';
 import Container from './styled';
 
-const PageContainer = ({ 
-    children, 
-    loading, 
-  }) => {
+const PageContainer = ({children, loading}) => {
   return (
     <Container>
-      <Loader loading={loading}>
-        { children }
-      </Loader>
+      <Loader loading={loading}>{children}</Loader>
     </Container>
   );
 };
